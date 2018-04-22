32510113b2d1596730d3c57135c57021729d2339
Ujjawal Prasad <praujj@gmail.com>
2018-04-22 15:56:03 +0530

    Update train.py

--- train.py
+++ train.py
@@ -184,15 +184,15 @@ def main(_):
 	data_generator = prepare_data.get_batches(data, batch_size)
 
 	hidden_size = 256
-	encoder1 = EncoderRNN(input_lang.n_words, hidden_size)
-	attn_decoder1 = AttnDecoderRNN(hidden_size, output_lang.n_words, dropout_p=0.1)
+	encoder1 = Encoder(input_lang.n_words, hidden_size)
+	decoder1 = Decoder(hidden_size, output_lang.n_words, dropout_p=0.1)
 
 
 	if use_cuda:
 	    encoder1 = encoder1.cuda()
-	    attn_decoder1 = attn_decoder1.cuda()
+	    attn_decoder1 = decoder1.cuda()
 
-	trainIters(encoder1, attn_decoder1, 75000, print_every=5000)
+	trainIters(encoder1, decoder1, 75000, print_every=5000)
 
 
 if __name__ == '__main__':
